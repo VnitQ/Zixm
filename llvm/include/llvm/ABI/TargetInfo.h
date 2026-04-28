ff5977ac640a9d59c13353484c700603fae25f87
Narayan Sreekumar <nsreekumar6@gmail.com>
2026-04-29 01:56:09 +0530

    feat: Implement the SysV ABI for the LLVM ABI Library.

--- llvm/include/llvm/ABI/TargetInfo.h
+++ llvm/include/llvm/ABI/TargetInfo.h
@@ -16,7 +16,9 @@
 
 #include "llvm/ABI/FunctionInfo.h"
 #include "llvm/ABI/Types.h"
+#include "llvm/TargetParser/Triple.h"
 #include <cassert>
+#include <memory>
 
 namespace llvm {
 namespace abi {
@@ -82,6 +84,19 @@ protected:
 };
 
 std::unique_ptr<TargetInfo> createBPFTargetInfo(TypeBuilder &TB);
+
+/// The AVX ABI level for X86 targets.
+enum class X86AVXABILevel {
+  None,
+  AVX,
+  AVX512,
+};
+
+std::unique_ptr<TargetInfo> createX8664TargetInfo(TypeBuilder &TB,
+                                                  const Triple &TargetTriple,
+                                                  X86AVXABILevel AVXLevel,
+                                                  bool Has64BitPointers,
+                                                  const ABICompatInfo &Compat);
 
 } // namespace abi
 } // namespace llvm
